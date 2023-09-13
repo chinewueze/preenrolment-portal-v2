7030789476f56feefa224bb4f25e01ae0c1c884d
chinewueze <chiderauba83@gmail.com>
2023-09-13 22:30:23 +0100

    Added a feature to enable the button to redirect to home page once inputs are filled

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,8 +1,10 @@
 import React, { useEffect, useState } from 'react';
+import {useNavigate } from 'react-router-dom'
 import { LockOutlined, UserOutlined } from '@ant-design/icons';
 import { Button, Form, Input } from 'antd';
 import { Footer } from './footer';
 export const Login = () => {
+    const navigate = useNavigate()
     const [form] = Form.useForm();
     const [clientReady, setClientReady] = useState(false);
 
@@ -16,63 +18,62 @@ export const Login = () => {
     return (
         <div className='relative w-screen h-screen'>
             <header className="p-3 ">
-            <img src= "assets/plaschema-logo.png" className='ml-[3%]' />
+                <img src="assets/plaschema-logo.png" className='ml-[3%]' />
             </header>
             <main className=' pt-[4%]'>
                 <h1 className='text-center font-bold text-3xl'> SIGN IN </h1>
-            <Form
-                className=' bg-stone-100 rounded-lg w-fit mx-auto my-[6%] p-[2%] shadow-lg'
-                form={form} name="horizontal_login" layout="block" onFinish={onFinish}>
-                <Form.Item
-                    name="username"
-                    rules={[
-                        {
-                            required: true,
-                            message: 'Please input your username!',
-                        },
-                    ]}
-                >
-                    <Input
-                        className='w-96'
-                        prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Email Address" />
-                </Form.Item>
-                <Form.Item
-                    name="password"
-                    rules={[
-                        {
-                            required: true,
-                            message: 'Please input your password!',
-                        },
-                    ]}
-                >
-                    <Input
-                        prefix={<LockOutlined className="site-form-item-icon " />}
-                        className='w-96'
-                        type="password"
-                        placeholder="Password"
-                    />
-                </Form.Item>
-                <Form.Item shouldUpdate>
-                    {() => (
-                        <Button
-                            type="primary"
-                            htmlType="submit"
-                            className={` my-[2%] ${clientReady
-                                ? 'bg-green-500 text-white'
-                                : 'bg-blue-500 text-disabledText'
-                                }`}
+                <Form
+                    className=' bg-stone-100 rounded-lg w-fit mx-auto my-[3%] p-[2%] shadow-lg'
+                    form={form} name="horizontal_login" layout="block" onFinish={onFinish}>
+                    <Form.Item
+                        name="email"
+                        rules={[
+                            {
+                                required: true,
+                                message: 'Please input your email!',
+                            },
+                        ]}
+                    >
+                        <Input
+                            className='w-96'
+                            prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Email Address" />
+                    </Form.Item>
+                    <Form.Item
+                        name="password"
+                        rules={[
+                            {
+                                required: true,
+                                message: 'Please input your password!',
+                            },
+                        ]}
+                    >
+                        <Input.Password prefix={<LockOutlined className="site-form-item-icon " />}
+                            className='w-96'
+                            type="password"
+                            placeholder="Password" />
+                    </Form.Item>
+                    <Form.Item shouldUpdate>
+                        {() => (
+                            <Button
+                                onClick={() => {if (form.isFieldsTouched()) {navigate('/home')} }}
+                                type="primary"
+                                htmlType="submit"
+                                className={` my-[2%]  ${clientReady
+                                    ? 'bg-green-500 text-white'
+                                    : 'bg-blue-500 text-disabledText'
+                                    }`}
 
-                            disabled={
-                                !clientReady ||
-                                !form.isFieldsTouched(true) ||
-                                !!form.getFieldsError().filter(({ errors }) => errors.length).length
-                            }
-                        >
-                            Sign in
-                        </Button>
-                    )}
-                </Form.Item>
-            </Form>
+                                disabled={
+                                    !clientReady ||
+                                    !form.isFieldsTouched(true) ||
+                                    !!form.getFieldsError().filter(({ errors }) => errors.length).length
+                                }
+                            >
+                                Sign in
+                            </Button>
+                        )}
+                    </Form.Item>
+                </Form>
             </main>
             <Footer />
         </div>
