e573eb037186ffacab736bd5ce36df1d2572c802
chinewueze <chiderauba83@gmail.com>
2023-09-13 22:27:11 +0100

    Added a transition and transform property to the company's logo, changed the text in the logout button to an icon and ensured it redirects the user to the login page once it is clicked

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,34 +1,22 @@
 import React, { useState } from 'react';
-import { Typography } from 'antd';
-const { Title } = Typography;
-import { Button } from 'antd';
+import {useNavigate} from 'react-router-dom'
+import { LogoutOutlined } from '@ant-design/icons';
+
+;
+
 export const Header = () => {
-    const [loadings, setLoadings] = useState([]);
-    const enterLoading = (index) => {
-        setLoadings((prevLoadings) => {
-            const newLoadings = [...prevLoadings];
-            newLoadings[index] = true;
-            return newLoadings;
-        });
-        setTimeout(() => {
-            setLoadings((prevLoadings) => {
-                const newLoadings = [...prevLoadings];
-                newLoadings[index] = false;
-                return newLoadings;
-            });
-        }, 6000);
-    };
+    const navigate = useNavigate()
     return (
         <header className="w-full flex  h-[13vh] bg-green-500 p-4 ">
             <div className=" ml-[1%] ">
-                <img src="assets/plashema-logo.jpg" className=" h-[9vh] w-[215px] rounded-2xl" />
+                <img src="assets/plashema-logo.jpg" className=" h-[9vh] w-[215px] rounded-2xl delay-150 duration-700 ease-in-out hover:scale-110" />
             </div>
             <div className=' w-[65%]  ml-[5%] mr-[10%] text-center'>
                 <h1 className=' text-2xl text-white mt-5 font-bold '> Plateau Health Pre-Enrollment Portal </h1>
             </div>
-            <Button className='h-11 p-2 bg-white self-center text-black font-semibold' type="primary" loading={loadings[0]} onClick={() => enterLoading(0) }>
-                    logout
-            </Button>
+            <button className=' rounded-lg text-blue-500 bg-white p-2 text-4xl self-center outline-none active:bg-red-400' onClick={() => navigate("/")} >
+                <LogoutOutlined />
+            </button>
         </header>
     )
 } 
