958ff2e4d301a38f59bf98aad0dd93941e25c3b2
chinewueze <chiderauba83@gmail.com>
2023-09-13 22:28:45 +0100

    Changed the copyright symbol to the entity code

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,7 +1,7 @@
 export const Footer = () => {
     return (
         <footer className="absolute bottom-0 p-1.5 w-full bg-neutral-100">
-            <h3 className="text-center"> Copyright © Designed for Plateau State Contributory Health Care Management Agency by <a href="/https://instantdeposit.africa/" className="font-semibold text-lg" > Instant Deposit Ltd</a>. All Rights Reserved.2023.</h3>
+            <h3 className="text-center"> Copyright &copy; Designed for Plateau State Contributory Health Care Management Agency by <a href="https://instantdeposit.africa/"  target="_blank" className="font-semibold text-lg" > Instant Deposit Ltd</a>. All Rights Reserved.2023.</h3>
         </footer>
     )
 }   
