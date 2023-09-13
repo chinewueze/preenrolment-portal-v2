992189b1179b32c89e0484bca8831381c3c50a94
chinewueze <chiderauba83@gmail.com>
2023-09-13 22:20:31 +0100

    Assigned paths for proper routing across components

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 // import { useState } from 'react'
 import { Route, Routes } from "react-router-dom";
 import { Login } from "./components/login";
-import { Home } from "./homepage";
+import { Home } from "./components/homepage";
+import {Portal} from "./components/portal"
 export default function App() {
 
   return (
     <>
       <Routes>
-        {/* <Route path="/" element={<Login />} /> */}
-        <Route path="/" element={<Home/>} />
+        <Route path="/" element={<Login />} />
+        <Route path="/home" element={<Home/>} />
+        <Route path="/portal" element={<Portal/>} />
       </Routes>
     </>
   )
