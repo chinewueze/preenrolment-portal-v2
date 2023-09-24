280206a98a06a6769c1763bf2ddad69d98a5034f
chinewueze <chiderauba83@gmail.com>
2023-09-24 23:34:43 +0100

    made the date more dynamic

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,7 +1,9 @@
 export const Footer = () => {
     return (
-        <footer className=" p-1.5 w-full bg-neutral-100">
-            <h3 className="text-center"> Copyright &copy; Designed for Plateau State Contributory Health Care Management Agency by <a href="https://instantdeposit.africa/"  target="_blank" className="font-semibold text-lg" > Instant Deposit Ltd</a>. All Rights Reserved.2023.</h3>
-        </footer>
-    )
+        <footer className=' w-full absolute bottom-0'>
+            <div className=" p-1.5 w-full bg-neutral-100">
+                <h3 className="text-center"> Copyright &copy; Designed for Plateau State Contributory Health Care Management Agency by <a href="https://instantdeposit.africa/" target="_blank" className="font-semibold text-lg" > Instant Deposit Ltd</a>. All Rights Reserved. {new Date().getFullYear()}.</h3>
+            </div>
+            </footer>
+                )
 }   
