f69c34aab28d25b51af2c13a90e53b994375f5ca
chinewueze <chiderauba83@gmail.com>
2023-09-25 21:37:56 +0100

    Added a list of healthcare organizations (HMOs) as a data source.

---
+++ src/components/FormData.js
@@ -0,0 +1,15 @@
+const hmo = [
+    { key: "0", value: "Ashmed" },
+    { key: "1", value: "Infinite X2" },
+    { key: "2", value: "Markfema" },
+    { key: "3", value: "Precious Health" },
+    { key: "4", value: "ProHealth" },
+    { key: "5", value: "Regenix" },
+    { key: "6", value: "First Care" },
+    { key: "7", value: "Salus Trust" },
+    { key: "8", value: "United Comprehensive" },
+    { key: "9", value: "United Health" },
+    { key: "10", value: "Well Health" },
+    { key: "11", value: "Plaschema" },
+  ];
+  export {hmo} 
