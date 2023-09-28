8884f47090c41c7737c07fc7db6531c9e50227f1
chinewueze <chiderauba83@gmail.com>
2023-09-28 13:32:39 +0100

    Updated footer styling

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,6 +1,6 @@
 export const Footer = () => {
     return (
-        <footer className=' w-full absolute bottom-0'>
+        <footer className='h-[7%] m-0 w-full absolute bottom-0'>
             <div className=" p-1.5 w-full bg-neutral-100">
                 <h3 className="text-center"> Copyright &copy; Designed for Plateau State Contributory Health Care Management Agency by <a href="https://instantdeposit.africa/" target="_blank" className="font-semibold text-lg" > Instant Deposit Ltd</a>. All Rights Reserved. {new Date().getFullYear()}.</h3>
             </div>
